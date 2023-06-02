916232fde5fe3fb666b2979753a853ff6dbd920a
bradhead <brad.head@gmail.com>
2023-06-02 16:48:58 -0700

    update struct.

---
+++ docs/index.md
@@ -0,0 +1,7 @@
+---
+page_title: "Smile CDR Provider"
+---
+
+# Smile CDR Provider
+
+The Smile CDR provider can be used to interact with [Smile CDR](https://www.smilecdr.com/).
---
+++ provider/provider.go
@@ -0,0 +1,11 @@
+package provider
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{},
+	}
+}
