0bfc0d8362262fb403cfaebd6bc05f541bab9476
bradhead <brad.head@gmail.com>
2023-07-10 17:23:28 -0700

    fadsfasdf

---
+++ smilecdr/README.md
@@ -0,0 +1,9 @@
+# Smile CDR JSON Admin API 
+
+Smile CDR includes an administration API based on RESTful JSON Web Services.  The API uses simple JSON-based REST calls to check status and configure the CDR.
+
+The default base URL for this API will be found on port 9000 and would be accessible on a localhost installation at http://localhost:9000
+
+## For More details
+
+See [Smile CDR Admin API](https://smilecdr.com/docs/json_admin_endpoints/json_admin_api.html)
--- smilecdr/openid_client.go
+++ smilecdr/openid_client.go
@@ -1,5 +1,38 @@
 package smilecdr
 
+type ClientSecret struct {
+	Pid         int    `json:"pid,omitempty"`
+	Secret      string `json:"secret,omitempty"`
+	Description string `json:"description,omitempty"`
+	Expiration  string `json:"expiration,omitempty"`
+	activation  string `json:"activation,omitempty"`
+}
+
 type OpenIdClient struct {
-	Pid int `json:"pid,omitempty"`
+	Pid                         int           `json:"pid,omitempty"`
+	NodeId                      string        `json:"nodeId,omitempty"`
+	ModuleId                    string        `json:"moduleId,omitempty"`
+	AccessTokenValiditySeconds  int           `json:"accessTokenValiditySeconds,omitempty"`
+	AllowedGrantTypes           []string      `json:"allowedGrantTypes,omitempty"`
+	AutoApproveScopes           []string      `json:"autoApproveScopes,omitempty"`
+	AutoGrantScopes             []string      `json:"autoGrantScopes,omitempty"`
+	ClientId                    string        `json:"clientId,omitempty"`
+	ClientName                  string        `json:"clientName,omitempty"`
+	ClientSecrets               *ClientSecret `json:"clientSecrets,omitempty"`
+	FixedScope                  bool          `json:"fixedScope,omitempty"`
+	RefreshTokenValiditySeconds int           `json:"refreshTokenValiditySeconds,omitempty"`
+	RegisteredRedirectUris      []string      `json:"registeredRedirectUris,omitempty"`
+	Scopes                      []string      `json:"scopes,omitempty"`
+	SecretRequired              bool          `json:"secretRequired,omitempty"`
+	SecretClientCanChange       bool          `json:"secretClientCanChange,omitempty"`
+	Enabled                     bool          `json:"enabled,omitempty"`
+	CanIntrospectAnyTokens      bool          `json:"canIntrospectAnyTokens,omitempty"`
+	CanIntrospectOwnTokens      bool          `json:"canIntrospectOwnTokens,omitempty"`
+	AlwaysRequireApproval       bool          `json:"alwaysRequireApproval,omitempty"`
+	CanReissueTokens            bool          `json:"canReissueTokens,omitempty"`
+	Permissions                 []string      `json:"permissions,omitempty"`
+	AttestationAccepted         bool          `json:"rememberedScopes,omitempty"`
+	PublicJwksUri               string        `json:"publicJwksUri,omitempty"`
+	ArchivedAt                  string        `json:"archivedAt,omitempty"`
+	CreatedByAppSphere          string        `json:"createdByAppSphere,omitempty"`
 }
