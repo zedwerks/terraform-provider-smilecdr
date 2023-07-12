46d97b4788ecf022ee4c2827c81071091a457814
bradhead <brad.head@gmail.com>
2023-07-12 15:05:01 -0700

    add basic openid client example

---
+++ example/main.tf
@@ -0,0 +1,81 @@
+# Copyright (c) Zed Werks Inc.
+# SPDX-License-Identifier: Apache-2.0
+  
+terraform {
+  required_providers {
+    smilecdr = {
+        source = "zedwerks/smilecdr"
+        version = "0.1.0"
+    }
+  }
+}
+
+provider "smilecdr" {
+    url = "http://localhost:8000"
+    username = "admin"
+    password = "password"
+}
+
+
+
+resource "smilecdr_openid_client" "client1" {
+
+    nodeId = "Master"
+    moduleId = "smart_auth"
+    accessTokenValiditySeconds = 300
+    allowedGrantTypes = [
+        "authorization_code",
+        "refresh_token",
+        "client_credentials",
+        "password",
+        "implicit"
+    ]
+    autoApproveScopes = [
+        "openid",
+        "profile",
+        "email",
+        "patient/*.*",
+        "launch",
+        "launch/patient",
+        "launch/encounter",
+        "offline_access"
+    ]
+    autoGrantScopes = [
+        "openid",
+        "profile",
+        "email",
+        "patient/*.*",
+        "launch",
+        "launch/patient",
+        "launch/encounter",
+        "offline_access"
+    ]
+    clientId = "client1"
+    clientName = "Client 1"
+    fixedScope = false
+    refreshTokenValiditySeconds = 86400
+    registeredRedirectUris = [ "http://localhost:3000" ]
+    scopes = [
+        "openid",
+        "profile",
+        "email",
+        "patient/*.*",
+        "launch",
+        "launch/patient",
+        "launch/encounter",
+        "offline_access"
+    ]
+    secretRequired = false
+    secretClientCanChange = false
+    enabled = true
+    canIntrospectAnyTokens = false
+    canIntrospectOwnTokens = false
+    alwaysRequireApproval = false
+    canReissueTokens = false
+    rememberApprovedScopes = false
+    attestationAccepted = false
+    publicJwks = ""
+    jwksUrl = ""
+    createdByAppSphere = false
+
+}
