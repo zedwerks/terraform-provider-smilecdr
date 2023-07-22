e3e387b91b3033d8c9f4721148737a1339887d97
bradhead <brad.head@gmail.com>
2023-07-22 11:56:48 -0700

    added second example oidc client

--- example/openid_clients.tf
+++ example/openid_clients.tf
@@ -55,3 +55,47 @@ resource "smilecdr_openid_client" "client_lra_demo" {
   attestation_accepted      = false
 }
 
+
+resource "smilecdr_openid_client" "client_phr" {
+  node_id                       = "Master"
+  module_id                     = "smart_auth"
+  access_token_validity_seconds = 300
+  allowed_grant_types = [
+    "AUTHORIZATION_CODE",
+    "REFRESH_TOKEN", ]
+  auto_approve_scopes = [
+    "openid",
+    "profile",
+    "fhirUser",
+    "launch",
+    "launch/patient",
+    "offline_access"
+  ]
+  auto_grant_scopes = [
+    "openid",
+    "offline_access"
+  ]
+  client_id                      = "client_phr"
+  client_name                    = "Client PHR Demo"
+  fixed_scope                    = false
+  refresh_token_validity_seconds = 86400
+  registered_redirect_uris       = ["http://example-phr.com:6000"]
+  scopes = [
+    "openid",
+    "profile",
+    "fhirUser",
+    "patient/*.read",
+    "launch",
+    "launch/patient",
+    "offline_access"
+  ]
+  secret_required           = false
+  secret_client_can_change  = false
+  enabled                   = true
+  can_introspect_any_tokens = false
+  can_introspect_own_tokens = false
+  always_require_approval   = false
+  can_reissue_tokens        = false
+  remember_approved_scopes  = false
+  attestation_accepted      = false
+}
